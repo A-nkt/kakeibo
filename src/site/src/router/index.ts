0f69138b3c1ddee17f53ad842565bc6208680694
A-nkt <dsduoa31@gmail.com>
2026-01-20 19:27:02 +0900

    - dashboardのエンドポイントを作成 - localhost:3000で立ち上げるように変更

--- src/site/src/router/index.ts
+++ src/site/src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '@/views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard',
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+    },
+  ],
 })
 
 export default router
--- src/site/src/views/DashboardView.vue
+++ src/site/src/views/DashboardView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <div class="dashboard">
+    <h1>Dashboard</h1>
+  </div>
 </template>
 
 <style scoped></style>
