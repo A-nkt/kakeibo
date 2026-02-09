9f10770be33dd50b4911819310621ef328060f3c
A-nkt <dsduoa31@gmail.com>
2026-02-09 13:14:38 +0900

    ConstParameterクラスのインポートを修正し、AWS Lambda関数のCodeUriとHandlerを更新

--- src/api/site/src/app/values/__init__.py
+++ src/api/site/src/app/values/__init__.py
@@ -1,12 +1,12 @@
-import os
+from __future__ import annotations
 
+import os
 
 
 class ConstParameter:
     ITEM_TABLE_NAME = os.environ['ITEM_TABLE_NAME']
     CATEGORY_TABLE_NAME = os.environ['CATEGORY_TABLE_NAME']
     CUSTOMER_TABLE_NAME = os.environ['CUSTOMER_TABLE_NAME']
-
 
 
 Const = ConstParameter()
